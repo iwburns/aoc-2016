47d901936e0d29528d35dc24497c8d1628ca45fa
Ian Burns <iwburns8@gmail.com>
2016-12-07 22:51:42 -0600

    clean up 7-1

--- day-7-1/src/main.rs
+++ day-7-1/src/main.rs
@@ -7,49 +7,58 @@ fn main() {
     let mut num_found = 0u32;
 
     'a: for line in input.lines() {
-        let line_parts: Vec<&str> = line.split('[').collect();
-        for line_part in line_parts {
+        let (outside_brackets, inside_brackets) = split_line_by_brackets(line);
 
-            let bracket_split: Vec<&str> = line_part.split(']').collect();
-            if bracket_split.len() > 1 {
-                // [0] is the part inside the brackets.
-                for window in bracket_split[0].chars().collect::<Vec<char>>().windows(4) {
-                    if window[0] == window[3] && window[1] == window[2] && window[0] != window[1] {
-                        // don't count this line, we found an abba section in the brackets.
-                        continue 'a;
-                    }
-                }
-            }
-        }
-        let line_parts: Vec<&str> = line.split('[').collect();
-        for line_part in line_parts {
-
-            let bracket_split: Vec<&str> = line_part.split(']').collect();
-            if bracket_split.len() > 1 {
-                // [1] is the part inside the brackets.
-                for window in bracket_split[1].chars().collect::<Vec<char>>().windows(4) {
-                    if window[0] == window[3] && window[1] == window[2] && window[0] != window[1] {
-                        num_found += 1;
-                        // skip the rest of this line, we don't want to over count
-                        continue 'a;
-                    }
-                }
-            } else {
-                // [0] is the part inside the brackets.
-                for window in bracket_split[0].chars().collect::<Vec<char>>().windows(4) {
-                    if window[0] == window[3] && window[1] == window[2] && window[0] != window[1] {
-                        num_found += 1;
-                        // skip the rest of this line, we don't want to over count
-                        continue 'a;
-                    }
-                }
+        for section in inside_brackets {
+            let abbas = get_abbas(section);
+            if abbas.len() > 0 {
+                continue 'a;
             }
         }
 
+        for section in outside_brackets {
+            let abbas = get_abbas(section);
+            if abbas.len() > 0 {
+                num_found += 1;
+                continue 'a;
+            }
+        }
     }
 
     println!("num_found: {}", num_found);
+}
 
+fn get_abbas(string: &str) -> Vec<String> {
+    let mut abbas = Vec::<String>::new();
+
+    for window in string.chars().collect::<Vec<char>>().windows(4) {
+        if is_abba_window(window) {
+            abbas.push(window.iter().cloned().collect());
+        }
+    }
+
+    abbas
+}
+
+fn is_abba_window(window: &[char]) -> bool {
+    window[0] == window[3] && window[1] == window[2] && window[0] != window[1]
+}
+
+fn split_line_by_brackets(line: &str) -> (Vec<&str>, Vec<&str>) {
+    let parts: Vec<&str> = line.split(|x| x == '[' || x == ']').collect();
+
+    let mut outside_parts: Vec<&str> = Vec::new();
+    let mut inside_parts: Vec<&str> = Vec::new();
+
+    for i in 0..parts.len() {
+        if i % 2 == 0 {
+            outside_parts.push(parts[i]);
+        } else {
+            inside_parts.push(parts[i]);
+        }
+    }
+
+    (outside_parts, inside_parts)
 }
 
 fn get_input_string() -> Result<String, std::io::Error> {
